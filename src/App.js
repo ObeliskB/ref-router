a77a1111cd9891c230f3579cd09a0045ae215d31
obeliskB <anusorn.b@gmail.com>
2023-07-25 10:02:19 +0700

    create react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import React, { useRef } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Container, Button } from "react-bootstrap";
+import { Container, Button, Table } from "react-bootstrap";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import Contact from "./Contact";
 
@@ -45,20 +46,71 @@ export function Layout() {
 }
 
 function Index() {
+  const header1 = useRef();
   return (
     <>
       <Layout />
-      <h3>Hello</h3>
-      <Button>Click Me</Button>
+      <h3 ref={header1}>Hello</h3>
+      <Button
+        onClick={() => {
+          header1.current.innerHTML = "React";
+        }}
+      >
+        Click Me
+      </Button>
     </>
   );
 }
 
 function Product() {
+  const table = useRef();
+  const tr = useRef([]);
+  const DeleteRow = (i) => {
+    const index = tr.current[i].rowIndex;
+    table.current.deleteRow(index);
+  };
+
+  const data = [
+    ["กางเกงยีนส์", 1200],
+    ["เสื้อยืด", 350],
+    ["กางเกงขาสั้น", 450],
+    ["หมวก", 500],
+    ["ถุงเท้า", 100],
+  ];
+
   return (
     <>
       <Layout />
       <h4>Products</h4>
+      <Table striped bordered hover className="my-3" ref={table}>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, i) => {
+            return (
+              <tr
+                ref={(el) => {
+                  tr.current[i] = el;
+                }}
+                key={i}
+              >
+                <td>{item[0]}</td>
+                <td>{item[1]}</td>
+                <td className="text-center">
+                  <Button variant="danger" onClick={() => DeleteRow(i)}>
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
     </>
   );
 }
